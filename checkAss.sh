56e4bf4196f9820ecfd84afff01e8bb5b88d6573
adam <adam.brown@soho66.co.uk>
2018-04-03 16:53:25 +0100

    check script updated for new startup

--- checkAss.sh
+++ checkAss.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 a=$(pgrep -f __main | wc -l)
 [ "a$a" == "a0" ] && /home/pi/Assistant/startGoogleAss.sh &
-[ $a -gt 1 ] && $(pkill goog) && /home/pi/Assistant/startGoogleAss.sh &
+[ $a -gt 1 ] && $(pkill -f __main) && /home/pi/Assistant/startGoogleAss.sh &
