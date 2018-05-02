3285d56ca1426cb588834a4f559f3f77c205d8e8
adam <strictly_prohibited@hotmail.com>
2018-05-02 22:49:08 +0100

    added fridge

--- startGoogleAss.sh
+++ startGoogleAss.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /home/pi/env/bin/activate >> /home/pi/assLogs 
-/home/pi/Assistant/__main__.py >> /home/pi/assLogs
+source /home/pi/env/bin/activate >> /home/pi/assLogs 2>&1
+/home/pi/Assistant/__main__.py >> /home/pi/assLogs 2>&1
