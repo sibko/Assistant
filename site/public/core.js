a7c2cc365eed5a26369aecdfe3b24aa1e70c1289
adam <strictly_prohibited@hotmail.com>
2018-05-17 00:23:05 +0100

    added cat laser

--- site/public/core.js
+++ site/public/core.js
@@ -34,11 +34,33 @@ deviceControl.controller("MainController", ['$scope', '$http', '$uibModal', func
 	}
 
 	$scope.openDeviceModal = function (device) {
+		console.log(device)
+		if(device.type=='CatLaser'){
+			return $scope.openCatLaserModal(device);
+		}
 		$scope.modalInstance = $uibModal.open({
 			ariaLabelledBy: 'modal-title',
 			ariaDescribedBy: 'modal-body',
 			templateUrl: 'deviceModal.html',
 			controller: 'DeviceHandlerController',
+			controllerAs: '$ctrl',
+			size: 'lg',
+			resolve: {
+				device: function () {
+					return device
+				}
+			}
+		});
+
+	}
+
+	$scope.openCatLaserModal = function (device) {
+		console.log("catLaser")
+		$scope.modalInstance = $uibModal.open({
+			ariaLabelledBy: 'modal-title',
+			ariaDescribedBy: 'modal-body',
+			templateUrl: 'CatLaserModal.html',
+			controller: 'CatLaserController',
 			controllerAs: '$ctrl',
 			size: 'lg',
 			resolve: {
@@ -102,6 +124,25 @@ deviceControl.controller("TimerHandlerController", function ($scope, $http, $uib
 	$scope.minutes = 0;
 	$scope.createTimer = function (device, action) {
 		$http.get('/api/device/' + device.name + '/' + action + '/' + $scope.minutes)
+			.then(function (data) {
+				console.log("action complete", data);
+			}, function (error) {
+				console.log('Error: ' + error);
+			});
+	}
+
+	$scope.closeModal = function () {
+		$uibModalInstance.close();
+	}
+
+});
+
+deviceControl.controller("CatLaserController", function ($scope, $http, $uibModal, $uibModalInstance, device) {
+	console.log("modal", device)
+	$scope.device = device
+	
+	$scope.setValue = function (key, value) {
+		$http.get('http://' + device.ids[0] + '/?' + key + '=' + value)
 			.then(function (data) {
 				console.log("action complete", data);
 			}, function (error) {
