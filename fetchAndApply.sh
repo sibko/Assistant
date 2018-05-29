88516850be4943abc85a1ada5b5aabb6e9d3fe4b
adam <strictly_prohibited@hotmail.com>
2018-05-30 00:57:51 +0100

    added startup and pull logging

--- fetchAndApply.sh
+++ fetchAndApply.sh
@@ -2,5 +2,6 @@
 
 cd /home/pi/Assistant
 git pull
+echo "$(date) PULLED LATEST CHANGES" >> /home/pi/assLogs.log
 pkill -f __main
 [ "$(hostname)" == "bedroomAssistant" ] && service DeviceList restart
--- startGoogleAss.sh
+++ startGoogleAss.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo "$(date) STARTING UP ASSISTANT" >> /home/pi/assLogs.log
 source /home/pi/env/bin/activate >> /home/pi/assLogs.log 2>&1
 /home/pi/Assistant/__main__.py >> /home/pi/assLogs.log 2>&1
