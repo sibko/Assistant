6748212039c0522ffec218236b27d38eb23b60fb
adam <strictly_prohibited@hotmail.com>
2018-09-10 19:06:37 +0100

    updated fetchandapply script

--- fetchAndApply.sh
+++ fetchAndApply.sh
@@ -3,10 +3,12 @@
 cd /home/pi/Assistant
 git pull
 echo "$(date) PULLED LATEST CHANGES" >> /home/pi/assLogs.log
-cp ./assistant.service /etc/systemd/system/assistant.service
+cp -f ./serviceFiles/assistant.service /etc/systemd/system/assistant.service
+cp -f ./serviceFiles/mplayerServer.service /etc/systemd/system/mplayerServer.service
 echo "copied"
 systemctl daemon-reload
 echo "reloaded"
 service assistant restart
+service mplayerServer restart
 echo "restarted"
 [ "$(hostname)" == "bedroomAssistant" ] && service DeviceList restart
