f6054cfad49fd5300083a52bb6f7528e57fbac36
root <root@Microserver.home>
2019-12-04 20:37:57 +0000

    Christmas lights added@

--- updateAll.sh
+++ updateAll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for host in "loungeAssistant" "bedroomAssistant" "kitchenAssistant" "diningAssistant" "sittingRoomAssistant"; do
 	echo "updating $host"
-	echo "sudo /home/pi/Assistant/fetchAndApply.sh" | ssh pi@$host
+	echo "sudo /home/pi/Assistant/fetchAndApply.sh" | ssh pi@$host.local
 done
