f5adeb542a0b2e13d141f91336ec2857769e4f70
sibko <root@Microserver.home>
2020-11-18 19:42:37 +0000

    pingscript and updateall updated

--- updateAll.sh
+++ updateAll.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-hosts=$(cat /home/sibko/Assistant/config.json | jq '.devices'  | jq '.[] | select(.name | contains("Assistant")) | select(.type | contains("rPI")) | .ip' | tr -d \" )
+hosts=$(cat /home/sibko/Assistant/config.json | jq '.devices'  | jq '.[] | select(.name | contains("Assistant")) | select(.type | contains("rPI")) | select(.hidden == null) | .ip' | tr -d \" )
 for host in $hosts; do
+	echo "-----------------------------------------------------------------------------------------"
 	echo "updating $host"
 	echo "sudo /home/pi/Assistant/fetchAndApply.sh" | ssh pi@$host
+	echo ""
+	echo ""
 done
