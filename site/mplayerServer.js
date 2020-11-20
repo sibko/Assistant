651a90b50224454b4783cd2945fc8d48dba77620
sibko <root@Microserver.home>
2020-11-20 17:45:58 +0000

    disable video on mplayerServer (an mp3 was detected as having video and died)

--- site/mplayerServer.js
+++ site/mplayerServer.js
@@ -203,6 +203,7 @@ var startMplayer = function(file, additionalParams){
 	mplayerArgs.push('-quiet')
 	mplayerArgs.push('-really-quiet')
 	mplayerArgs.push('-volume')
+	mplayerArgs.push('-novideo')
 	mplayerArgs.push(globalVolume)
 
 
