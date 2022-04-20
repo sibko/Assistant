b94d7a2e187122ed940aa9f4e0c6da3f427d3c0f
sibko <strictly_prohibited@hotmail.com>
2022-04-20 16:07:13 +0100

    fixSound now correcting for pz2's

--- fixSound.sh
+++ fixSound.sh
@@ -2,39 +2,49 @@
 piasound="/home/pi/.asoundrc"
 rootasound="/etc/asound.conf"
 auxID=$(cat /proc/asound/modules | grep bcm2835 | cut -d ' ' -f 2)
+if [[ "$(hostname)" == "frontRoomAssistant" || "$(hostname)" == "sittingRoomAssistant" ]]; then
+        auxID=$(aplay -l | grep "USB Audio" | cut -d ' ' -f 2 | cut -d ':' -f 1)
+        speaker=$auxID
+fi
 currentID=$(cat $rootasound | grep "pcm.speaker" -A 3| tail -n 1 | cut -d ':' -f 2 | cut -d '"' -f 1)
 if [[ "$auxID" == "" ]]; then
         exit
 fi
 if [[ "$auxID,0" != "$currentID" ]] ; then
-        sed "s/$currentID/111,0/g; s/$auxID,0/$currentID/g; s/111,0/$auxID,0/g" $rootasound -i
+        sed "s/$currentID/999,0/g; s/$auxID,0/$currentID/g; s/999,0/$auxID,0/g" $rootasound -i
 fi
 
-auxID=$(cat /proc/asound/modules | grep bcm2835 | cut -d ' ' -f 2)
 currentID=$(cat $piasound | grep "pcm.speaker" -A 3| tail -n 1 | cut -d ':' -f 2 | cut -d '"' -f 1)
 if [[ "$auxID" == "" ]]; then
         exit
 fi
 if [[ "$auxID,0" != "$currentID" ]] ; then
-        sed "s/$currentID/111,0/g; s/$auxID,0/$currentID/g; s/111,0/$auxID,0/g" $piasound -i
+        sed "s/$currentID/999,0/g; s/$auxID,0/$currentID/g; s/999,0/$auxID,0/g" $piasound -i
 fi
 
-
 auxID=$(cat /proc/asound/modules | grep snd_usb_audio | cut -d ' ' -f 2| head -n 1)
+if [[ "$speaker" != "" ]]; then
+
+        auxID=$(cat /proc/asound/modules | grep snd_usb_audio | cut -d ' ' -f 2| grep -v "$speaker" | head -n 1)
+fi
+
 currentID=$(cat $piasound | grep "pcm.mic" -A 3| tail -n 1 | cut -d ':' -f 2 | cut -d '"' -f 1)
 if [[ "$auxID" == "" ]]; then
         exit
 fi
 if [[ "$auxID,0" != "$currentID" ]] ; then
-        sed "s/$currentID/111,0/g; s/$auxID,0/$currentID/g; s/111,0/$auxID,0/g" $piasound -i
+        sed "s/$currentID/999,0/g; s/$auxID,0/$currentID/g; s/999,0/$auxID,0/g" $piasound -i
 fi
 
 auxID=$(cat /proc/asound/modules | grep snd_usb_audio | cut -d ' ' -f 2| tail -n 1)
+if [[ "$speaker" != "" ]]; then
+        auxID=$(cat /proc/asound/modules | grep snd_usb_audio | cut -d ' ' -f 2| grep -v "$speaker" | head -n 1)
+fi
 currentID=$(cat $rootasound | grep "pcm.mumblemic" -A 3| tail -n 1 | cut -d ':' -f 2 | cut -d '"' -f 1)
 if [[ "$auxID" == "" ]]; then
         exit
 fi
 if [[ "$auxID,0" != "$currentID" ]] ; then
-        sed "s/$currentID/111,0/g; s/$auxID,0/$currentID/g; s/111,0/$auxID,0/g" $rootasound -i
+        sed "s/$currentID/999,0/g; s/$auxID,0/$currentID/g; s/999,0/$auxID,0/g" $rootasound -i
 fi
 
